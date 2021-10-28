38cd29e434d194e021f3043a7f7895372cabf75d
Kim Lauwers <kim.lauwers@acerta.be>
2021-10-28 15:50:16 +0200

    Enum

---
+++ src/main/java/kata/mortalkombat/tournament/Technique.java
@@ -0,0 +1,15 @@
+package kata.mortalkombat.tournament;
+
+public enum Technique {
+    MAE_GERI,
+    HIZA_GERI,
+    MAWASHI_GERI,
+    URA_MAWASHI_GERI,
+    TOBI_GERI,
+    USHIRO_GERI,
+    YOKO_GERI,
+    KIZAMI_TSUKI,
+    GYAKU_TSUKI,
+    OI_TSUKI
+
+}
