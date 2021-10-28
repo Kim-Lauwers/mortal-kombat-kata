b0c29f4989b6dcd4330ba3bb7eacbc013aa2b089
dev_grp <groeipakket_dev@kindengezin.be>
2021-10-28 16:00:42 +0200

    Add techniques

--- src/main/java/kata/mortalkombat/tournament/Fighter.java
+++ src/main/java/kata/mortalkombat/tournament/Fighter.java
@@ -1,17 +1,25 @@
 package kata.mortalkombat.tournament;
 
+import java.util.HashSet;
+import java.util.Set;
+
 class Fighter {
     private final String name;
     private final Realm realm;
+    private final Set<Technique> techniques;
 
     private Fighter(String name) {
         this.name = name;
         this.realm = Realm.getRandomRealm();
-
+        this.techniques = new HashSet<>();
     }
 
     static Fighter createFearsomeFighter(String name) {
         return new Fighter(name);
+    }
+
+    public void learnTechnique(Technique technique) {
+        this.techniques.add(technique);
     }
 
     public String getName() {
@@ -20,5 +28,9 @@ class Fighter {
 
     public Realm getRealm() {
         return realm;
+    }
+
+    public Set<Technique> getTechniques() {
+        return techniques;
     }
 }
--- src/test/java/kata/mortalkombat/tournament/FighterTest.java
+++ src/test/java/kata/mortalkombat/tournament/FighterTest.java
@@ -1,21 +1,37 @@
 package kata.mortalkombat.tournament;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class FighterTest {
+
+    private Fighter fighter;
+
+    @BeforeEach
+    public void setup() {
+        fighter = Fighter.createFearsomeFighter("Liu Kang");
+    }
+
     @Test
     void whenCreateFearsomeFighter_ThenFearsomeFighterIsCreated() {
-        Fighter theHero = Fighter.createFearsomeFighter("Liu Kang");
 
-        assertThat(theHero.getName()).isEqualTo("Liu Kang");
+        assertThat(fighter.getName()).isEqualTo("Liu Kang");
     }
 
     @Test
     void whenCreateFearsomeFighter_ThenFighterHasARealm(){
-        Fighter fighter = Fighter.createFearsomeFighter("Bob");
 
         assertThat(fighter.getRealm()).isNotNull();
+    }
+
+    @Test
+    void whenLearnTechnique_ThenTechniqueIsAddedToSet() {
+
+        Technique technique = Technique.GYAKU_TSUKI;
+        fighter.learnTechnique(technique);
+
+        assertThat(fighter.getTechniques()).containsExactly(technique);
     }
 }
